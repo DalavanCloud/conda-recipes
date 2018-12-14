885d2cea10fb1f16ba18d851d22d17bb08651a64
Sander Niemeijer <niemeijer@stcorp.nl>
2018-12-14 13:16:46 +0100

    Use prerelease versions of CODA/HARP for testing

--- coda/build.sh
+++ coda/build.sh
@@ -11,17 +11,5 @@ cmake \
   -DHDF4_INCLUDE_DIR="$PREFIX/include" \
   -DHDF5_INCLUDE_DIR="$PREFIX/include"
 
-#./bootstrap
-
-#if [ "$(uname)" == Darwin ]; then
-#    export CFLAGS="-headerpad_max_install_names"
-#fi
-
-#./configure --prefix="$PREFIX" \
-#            --with-hdf4 HDF4_LIB="$PREFIX/lib" HDF4_INCLUDE="$PREFIX/include"\
-#            JPEG_LIB="$PREFIX/lib" ZLIB_LIB="$PREFIX/lib" \
-#            --with-hdf5 HDF5_LIB="$PREFIX/lib" HDF5_INCLUDE="$PREFIX/include" \
-#            --enable-python PYTHON="$PREFIX/bin/python"
-
 make VERBOSE=1
 make install
